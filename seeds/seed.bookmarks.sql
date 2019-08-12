7b1157314bb98fb6583dd3ac915bd1f0e99be410
Lauren Auchter <lsauchter@gmail.com>
2019-08-11 21:34:10 -0400

    Added migration and seed

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title,url,rating,description) VALUES 
+('Thinkful','https://thinkful.com',5,'Learn to code')
+,('Google','https://google.com',5,'Find information')
+,('Meetup','https://meetup.com',4,'Meet people')
+,('Pinterest','https://pinterest.com',4,'Find inspiration')
+,('Facebook','https://facebook.com',3,'Keep up with friends')
+,('Twitter','https://twitter.com',4,'Follow people')
+,('Stack Overflow','https://stackoverflow.com',4,'Coding resources')
+,('MDN','https://developer.mozilla.org',5,'Documentation on web development')
+,('W3Schools','https://w3schools.com',3,'Tips and guides about coding')
+,('YouTube','https://youtube.com',4,'Video sharing platform')
+;
